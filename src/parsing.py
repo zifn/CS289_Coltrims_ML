e2881d81254d1eb1aceb2372eafa8c60b6587e1f
Richard T <zifn@users.noreply.github.com>
2020-10-26 19:07:28 -0700

    Parsing (#8)
    Overview
    ---------------
    Addresses issue #8 by implementing a pair of read/write functions that will either return/accept a numpy array or a pandas DataFrame. Headers are a mandatory input for these functions but a default set of values appropriate for 5 particle coincidence is given as a default input. The write function only checks if a numpy array is passed and assumes that if a numpy array wasn't passed then a pandas DataFrame was passed. Potentially this could result in future issues if other data structures are passed into this function as the error message might not end up being helpful.
    
    
    Commit History
    -------------------------
    
    * initial implementation of parsing module to read and write ' ' seperated momenta data files as well as tests for those read/write functions
    
    * fixing bugs in output txt file writting and typos in doc strings
    
    * increasing the number of integers used to check the read/write functionality
    
    * fixing linter errors
    
    * fixing typos in the doc strings
    
    * fixing coverage issue and logic bug in write momentum file
    
    * allowing the headers to be modifiable to allow for different momentum datasets with different numbers of columns to be read by the same function
    
    * fixing syntax error and linter error

---
+++ src/parsing.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+
+def read_momentum(input_file_path, return_numpy=True, has_headers=False,
+                                    headers=("Px_ion_1", "Py_ion_1", "Pz_ion_1",
+                                                    "Px_ion_2", "Py_ion_2", "Pz_ion_2",
+                                                    "Px_neutral", "Py_neutral", "Pz_neutral",
+                                                    "Px_elec_1", "Py_elec_1", "Pz_elec_1",
+                                                    "Px_elec_2", "Py_elec_2", "Pz_elec_2")):
+    """
+    Function to read input momentum data from a COLTRIMS dataset. Expects the
+    input file to be a ' ' delimited file representing the momenta of each particle
+    detected in coicidence.
+
+    Parameters
+    ------------
+    input_file_path : str or path-like
+        path to file with momentum information to read
+    return_numpy : bool
+        If true the function returns a numpy array else it returns a pandas dataframe
+    has_headers : bool
+        asks if the input file has headers or not. If the input file has a header on the
+        first line, the header names will be over written to the following names in
+        order:
+    headers : list of strings
+        a list of header names for the intermidiate or output dataframe
+
+    Returns
+    --------
+    array or dataframe
+            Returns an array or dataframe depending on the input parameters.
+            If a pandas dataframe is returned the headers will be those supplied
+            to the function.
+    """
+    if has_headers:
+        momentum_df = pd.read_csv(input_file_path,
+                                                    sep=" ",
+                                                    header=0,
+                                                    names=headers)
+    else:
+        momentum_df = pd.read_csv(input_file_path,
+                                                    sep=" ",
+                                                    header=None,
+                                                    names=headers)
+    if return_numpy:
+        return momentum_df.to_numpy()
+    else:
+        return momentum_df
+
+def write_momentum(output_file_path, momentum, write_headers=False,
+                                    headers=("Px_ion_1", "Py_ion_1", "Pz_ion_1",
+                                                    "Px_ion_2", "Py_ion_2", "Pz_ion_2",
+                                                    "Px_neutral", "Py_neutral", "Pz_neutral",
+                                                    "Px_elec_1", "Py_elec_1", "Pz_elec_1",
+                                                    "Px_elec_2", "Py_elec_2", "Pz_elec_2")):
+    """
+    Saves momenta data in either numpy array or dataframe format as a ' ' delimited
+    file.
+
+    Parameters
+    ------------
+    output_file_path: str or path-like
+        file location to save the input data
+    momentum : array or dataframe
+        data to save. Should be an n by 15 shaped numpy array or dataframe.
+    write_headers : bool
+        If true will write the following list as a header to the top of the file
+    headers : list of strings
+        a list of header names for the saved data file
+    """
+
+    if isinstance(momentum, np.ndarray):
+        if not write_headers:
+            np.savetxt(output_file_path, momentum, delimiter=' ')
+        else:
+            header_txt = ''
+            for header in headers:
+                header_txt += header + " "
+            np.savetxt(output_file_path, momentum, delimiter=' ', header=header_txt[:-1], comments='')
+    else: # assume momentum is a pandas dataframe
+        if write_headers:
+            momentum.to_csv(output_file_path, sep=" ", header=headers, index=False)
+        else:
+            momentum.to_csv(output_file_path, sep=" ", header=False, index=False)
---
+++ src/tests/test_parsing.py
@@ -0,0 +1,30 @@
+import os
+
+import numpy as np
+
+from .. import parsing
+
+def test_read_write():
+    temp_path = "temp.dat"
+    rng = np.random.default_rng()
+    values = rng.integers(0, 200, [200, 15])
+    headers = ["Px_ion_1", "Py_ion_1", "Pz_ion_1",
+                        "Px_ion_2", "Py_ion_2", "Pz_ion_2",
+                        "Px_neutral", "Py_neutral", "Pz_neutral",
+                        "Px_elec_1", "Py_elec_1", "Pz_elec_1",
+                        "Px_elec_2", "Py_elec_2", "Pz_elec_2"]
+
+    for write_headers in [False, True]:
+        for return_np in [True, False]:
+            print(f"write_headers = {write_headers}, return_np = {return_np}")
+            parsing.write_momentum(temp_path, values, write_headers)
+            read_values = parsing.read_momentum(temp_path, return_np, write_headers)
+            if not return_np:
+                read_headers = read_values.columns.to_list()
+                assert read_headers == headers
+                parsing.write_momentum(temp_path, read_values, write_headers)
+                read_values = parsing.read_momentum(temp_path, True, write_headers)
+            print("values.shape = ", values.shape)
+            print("read_values.shape = ", read_values.shape)
+            assert np.array_equal(values, read_values)
+            os.remove(temp_path)

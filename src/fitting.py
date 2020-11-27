617e584a270e90b6db6b087f09ecc3460b43d33e
zifn <zifn14@gmail.com>
2020-11-26 21:56:47 -0800

    added additional comments and fixed a typo

--- src/fitting.py
+++ src/fitting.py
@@ -39,7 +39,7 @@ def spherical_to_cart(M_sph):
     
     return M_xyz
 
-def sum_Y_lms_distribution(theta, phi, L_max, B_lms):
+def sum_Y_lms(theta, phi, L_max, B_lms):
     """
     Sum of Y_lms for spherical harmonics
     """
@@ -50,33 +50,60 @@ def sum_Y_lms_distribution(theta, phi, L_max, B_lms):
             accum += B_lms[L, M]*np.real(sph_harm(M, L, theta, phi))
     return accum
 
+def Y_lms_distribution(theta, phi, L_max, B_lms):
+    return sum_Y_lms(theta, phi, L_max, B_lms)/B_lms[0, 0]
+
 def SDA_Y_lms_distribution(M_xyz, L_max, eta=0.001, epochs=3, batch_fraction=0.01):
     """
-    Stochastic gradient assent update for Y_lm distribution
+    Stochastic gradient assent update for probability distribution based on spherical harmonics
+    
+    Parameters
+    ------------
+    M_xyz: n by 3 numpy array
+        observered data points representing the distribution in cartesian coordiantes 
+    L_max: int
+        Maximum value of L quantum number determing the number of terms to be used in the fits
+    eta: float > 0
+        learning rate/step size
+    epochs: int
+        number of times to cycle throught the data
+    batch_fraction: 1 >= float > 0
+        fraction of data points in each batch used in the SGD/SGA update
+    
+    Returns
+    --------
+    B_lms: 2D array
+        optimized B_lm coeficients used for the probability distribution
     """
     assert(M_xyz.shape[1] == 3)
-    rng = np.random.default_rng()
+    assert(batch_fraction <= 1 and batch_fraction > 0)
+    assert(eta > 0)
     
+    rng = np.random.default_rng()
+    # set up initial parameters and parameters that are called often
     N = M_xyz.shape[0]
     N_f = int(N*batch_fraction)
     B_lms = np.zeros([L_max, L_max])
     B_lms[0, 0] = 1
-    
+    # convert to spherical
     M_sph = cart_to_spherical(M_xyz)
     
     for i in range(epochs):
+        # set up new array of indices and choose a subset for the next batch
         indices = np.arange(0, N, 1)
         batch_ind = rng.choice(indices, size=N_f, replace=False)
         indices = np.delete(indices, batch_ind)
         for j in range(int(1/batch_fraction)):
-            M_sph_batch = M_sph[batch_ind]
-            F = sum_Y_lms_distribution(M_sph_batch[:,1], M_sph_batch[:,2], L_max, B_lms)
+            # compute the update
+            M_sph_batch = M_sph[batch_ind, :]
+            F = sum_Y_lms(M_sph_batch[:,1], M_sph_batch[:,2], L_max, B_lms)
             for L in range(0, L_max):
                 for M in range(0, L):
                     if L == 0 and M == 0:
                         B_lms[0, 0] -= eta*M_sph_batch.shape[0]/B_lms[0, 0]
                     f = np.real(sph_harm(M, L, M_sph_batch[:,1], M_sph_batch[:,2]))
                     B_lms[L, M] += eta*sum(f/F)
+            # choose next subset of indices for the next batch
             batch_ind = rng.choice(indices, size=N_f, replace=False)
             indices = np.delete(indices, batch_ind)
     return B_lms

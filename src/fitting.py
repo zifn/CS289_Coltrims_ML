80a0e98bc5e7b6bbfe905aa703793dc3bc55d98f
zifn <zifn14@gmail.com>
2020-11-27 23:59:35 -0800

    fixing linter errors

--- src/fitting.py
+++ src/fitting.py
@@ -3,160 +3,167 @@ import numpy as np
 
 def cart_to_spherical(M_xyz):
     """
-    Converts every pair of 3 columns of an input X numpy matrix from cartesian 
+    Converts every pair of 3 columns of an input X numpy matrix from cartesian
     to spherical coordinates.
+
+    Parameters
+    ------------
+    M_sph: ndarray (n, 3*m)
+        Expected order is X, Y, Z for each set of three columns in a row
+
+    Returns
+    --------
+    M_sph: ndarray (n, 3*m)
+        Returned order is R, Theta, Phi for each set of three columns in a row
     """
-    assert(M_xyz.shape[1]%3 == 0)
+    assert M_xyz.shape[1]%3 == 0
 
     M_sph = np.zeros(M_xyz.shape)
     for i in range(M_xyz.shape[1]//3):
         X = M_xyz[:,i*3]
         Y = M_xyz[:,i*3 + 1]
         Z = M_xyz[:,i*3 + 2]
-        
+
         R = np.sqrt(X**2 + Y**2 + Z**2) # R
         M_sph[:,i*3] = R
         M_sph[:,i*3 + 1] = np.arctan2(Y, X) # theta
         M_sph[:,i*3 + 2] = np.arccos(Z/R) # phi
-    
     return M_sph
-    
+
 def spherical_to_cart(M_sph):
     """
-    Converts every pair of 3 columns of an input X numpy matrix from cartesian 
-    to spherical coordinates.
+    Converts every pair of 3 columns of an input X numpy matrix from spherical
+    to cartesian scoordinates.
+
+    Parameters
+    ------------
+    M_sph: ndarray (n, 3*m)
+        Expected order is R, Theta, Phi for each set of three columns in a row
+
+    Returns
+    --------
+    M_xyz: ndarray (n, 3*m)
+        Returned order is X, Y, Z for each set of three columns in a row
     """
-    assert(M_sph.shape[1]%3 == 0)
+    assert M_sph.shape[1]%3 == 0
 
     M_xyz = np.zeros(M_sph.shape)
     for i in range(M_sph.shape[1]//3):
         R = M_sph[:,i*3]
         Theta = M_sph[:,i*3 + 1]
         Phi = M_sph[:,i*3 + 2]
-        
+
         M_xyz[:,i*3] = R*np.sin(Phi)*np.cos(Theta) # X
         M_xyz[:,i*3 + 1] = R*np.sin(Phi)*np.sin(Theta) # Y
         M_xyz[:,i*3 + 2] = R*np.cos(Phi) # Z
-    
     return M_xyz
 
-def sum_Y_lms(theta, phi, L_max, B_lms, only_even_Ls=False):
+def Y_lm_features(theta, phi, L_max, only_even_Ls=False):
     """
-    Sum of Y_lms for spherical harmonics
+    Make Y_lm feature matrix
+
+    Parameters
+    ------------
+    theta: 1D numpy array (n,)
+        array of theta angles ranging from 0 to 2*pi. Used to generate Y_lm
+        features
+    phi: 1D numpy array (n,)
+        array of phi angles ranging from 0 to pi. Used to generate Y_lm features.
+        Must be the same size as theta
+    L_max: int
+        maximum L value used to generate a truncated array of Y_lms
+    only_even_Ls: bool
+        if in the molecular frame, only use even L values to generate features
+
+    Returns
+    --------
+    Y_lm_features, lm_order: ndarray, list of tuples
+        Y_lm_features is the n by m real special harmonic feature matrix for
+        each l,m pair allowed by L_max. lm_order is a list of tuples where
+        each tuple is the associated (L, M) pair for each column in the Y_lm
+        feature matrix.
     """
-    assert(B_lms.shape[0] == L_max + 1 and B_lms.shape[1] == L_max + 1)
-    accum = np.zeros(len(theta))
+    Y_lm_feat = []
+    lm_order = []
     for L in range(0, L_max + 1):
         if only_even_Ls and L%2 == 1:
             continue
         for M in range(0, L + 1):
-            temp = B_lms[L, M]*np.real(sph_harm(M, L, theta, phi))
-            accum += temp
-    return abs(accum)
+            temp = np.real(sph_harm(M, L, theta, phi))
+            Y_lm_features.append(temp)
+            lm_order.append((L, M))
+    Y_lm_feat = np.array(Y_lm_features).T
+    return Y_lm_feat, lm_order
 
 def Y_lms_distribution(theta, phi, L_max, B_lms, only_even_Ls=False):
-    return sum_Y_lms(theta, phi, L_max, B_lms, only_even_Ls)/B_lms[0, 0]
+    """
+    Computes the probability density given a set of thetas and phis to be
+    sampled, an L_max to generate features and the associated array of coeficients.
+
+    Parameters
+    ------------
+    theta: 1D numpy array
+        array of theta angles ranging from 0 to 2*pi. Used to generate Y_lm features
+    phi: 1D numpy array
+        array of phi angles ranging from 0 to pi. Used to generate Y_lm features.
+        Must be the same size as theta
+    L_max: int
+        maximum L value used to generate a truncated array of Y_lms
+    B_lms: 1d numpy array
+        1d array of coeficients associated with a given real Y_lm
+    only_even_Ls: bool
+        if in the molecular frame, only use even L values to generate features
+
+    Returns
+    --------
+    float
+        probability density scaled by the B_00 coeficient
+    """
+    feats, _ = Y_lm_features(theta, phi, L_max, only_even_Ls)
+    product = feats @ B_lms
+    return abs(product)/B_lms[0]
 
 def fit_Y_lms_binning_least_squares(M_xyz, L_max, numb_bins, only_even_Ls=False):
     """
     Least Squares Fitting to get B_lm coeficients
-    
+
     Parameters
     ------------
     M_xyz: n by 3 numpy array
-        observered data points representing the distribution in cartesian coordiantes 
+        observered data points representing the distribution in cartesian coordiantes
     L_max: int
         Maximum value of L quantum number determing the number of terms to be used in the fits
     numb_bins: int
         number of bins in theta and phi axes
-    
+
     Returns
     --------
-    B_lms: 2D array
+    B_lms: 1d array
         optimized B_lm coeficients used for the probability distribution
+    lm_order: list of tuples
+        first index of each tuple is L and the second is M
     """
-    assert(M_xyz.shape[1] == 3)
-    assert(batch_fraction <= 1 and batch_fraction > 0)
+    assert M_xyz.shape[1] == 3
+    assert numb_bins > 0 and int(numb_bins) == numb_bins
     M_sph = cart_to_spherical(M_xyz)
-    theta = M_sph[:, 1]
-    phi = M_sph[:, 2]
-    
 
-def SDA_Y_lms_distribution(M_xyz, L_max, eta=0.001, epochs=3, batch_fraction=0.01, only_even_Ls=False):
-    """
-    Stochastic gradient assent update for probability distribution based on spherical harmonics
-    
-    Parameters
-    ------------
-    M_xyz: n by 3 numpy array
-        observered data points representing the distribution in cartesian coordiantes 
-    L_max: int
-        Maximum value of L quantum number determing the number of terms to be used in the fits
-    eta: float > 0
-        learning rate/step size
-    epochs: int
-        number of times to cycle throught the data
-    batch_fraction: 1 >= float > 0
-        fraction of data points in each batch used in the SGD/SGA update
-    
-    Returns
-    --------
-    B_lms: 2D array
-        optimized B_lm coeficients used for the probability distribution
-    """
-    raise NotImplementedError
-    assert(M_xyz.shape[1] == 3)
-    assert(batch_fraction <= 1 and batch_fraction > 0)
-    #assert(eta > 0)
-    
-    rng = np.random.default_rng()
-    # set up initial parameters and parameters that are called often
-    N = M_xyz.shape[0]
-    N_f = int(N*batch_fraction)
-    B_lms = np.zeros([L_max + 1, L_max + 1])
-    B_lms[0, 0] = 1
-    # convert to spherical
-    M_sph = cart_to_spherical(M_xyz)
-    
-    for i in range(epochs):
-        # set up new array of indices and choose a subset for the next batch
-        indices = list(range(0, N))
-        rng.shuffle(indices)
-        batch_ind = indices[:N_f]
-        indices = indices[N_f:]
-        for j in range(int(1/batch_fraction)):
-            # compute the update
-            M_sph_batch = M_sph[batch_ind, :]
-            F = sum_Y_lms(M_sph_batch[:,1], M_sph_batch[:,2], L_max, B_lms, only_even_Ls)
-            for L in range(0, L_max + 1):
-                if only_even_Ls and L%2 == 1:
-                    continue
-                for M in range(0, L + 1):
-                    if L == 0 and M == 0:
-                        B_lms[0, 0] -= eta*N_f/(B_lms[0, 0]*np.sqrt(4*np.pi))
-                        #continue #B_00 must be 1 after normalization
-                    f = np.real(sph_harm(M, L, M_sph_batch[:,1], M_sph_batch[:,2]))
-                    B_lms[L, M] += eta*sum(f/F)
-            # choose next subset of indices for the next batch
-            batch_ind = indices[:N_f]
-            indices = indices[N_f:]
-        #print(f"\nEpoch = {i} B_lms:\n {B_lms}")
-    return B_lms
-    
+    angular_hist, theta_hist, phi_hist = np.histogram2d(M_sph[:, 1], M_sph[:, 2], numb_bins,
+                                                                                    range=[[0, 2*np.pi], [0, np.pi]],
+                                                                                    density=True)
+    #estimate bin by center value
+    theta_hist = (theta_hist[1:] + theta_hist[:-1])/2
+    phi_hist = (phi_hist[1:] + phi_hist[:-1])/2
+    theta_hist, phi_hist = np.meshgrid(theta_hist, phi_hist)
 
+    # vectorize 2d histogram and input angles
+    angular_hist_flat = angular_hist.flatten()
+    theta_hist_flat = theta_hist.flatten()
+    phi_hist_flat = phi_hist.flatten()
 
+    # make ylm feature matrix
+    ylm_features, lm_order = Y_lm_features(theta_hist_flat, phi_hist_flat, L_max, only_even_Ls)
 
+    # solve for coeficients
+    B_lms = np.linalg.solve( ylm_features.T @ ylm_features, ylm_features.T @ angular_hist_flat)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return B_lms, lm_order
--- src/tests/test_fitting.py
+++ src/tests/test_fitting.py
@@ -11,7 +11,7 @@ def test_cart_spherical_conversions():
     if each other
     """
     rng = np.random.default_rng()
-    
+
     M_xyz_exp = np.array([[1, 0, 0],
                                       [0, 1, 0],
                                       [0, 0, 1]])
@@ -19,44 +19,48 @@ def test_cart_spherical_conversions():
     M_sph_exp = np.array([[1, 0, np.pi/2],
                                       [1, np.pi/2, np.pi/2],
                                       [1, 0, 0]])
-    
+
     M_sph_trans = fitting.cart_to_spherical(M_xyz_exp)
     assert np.allclose(M_sph_exp, M_sph_trans)
-    
+
     M_xyz_trans = fitting.spherical_to_cart(M_sph_trans)
     assert np.allclose(M_xyz_exp, M_xyz_trans)
-    
+
     M_xyz_rand = rng.standard_normal([200, 3])
     M_xyz_rand_trans = fitting.spherical_to_cart(fitting.cart_to_spherical(M_xyz_rand))
     assert np.allclose(M_xyz_rand, M_xyz_rand_trans)
-    
+
     M_xyz_rand = rng.standard_normal([200, 6])
     M_xyz_rand_trans = fitting.spherical_to_cart(fitting.cart_to_spherical(M_xyz_rand))
     assert np.allclose(M_xyz_rand, M_xyz_rand_trans)
-    
-def test_SDA_for_Y_lm_distribution():
+
+def test_fit_Y_lms_va_binning():
     """
-    Checks if SDA is able to find an equivalent probability distribution to a source distribution
+    Checks if least squares va binning is able to find an equivalent probability distribution to a source distribution
     """
     rng = np.random.default_rng()
     theta = np.linspace(0, 2*np.pi, 201)
     phi = np.linspace(0, np.pi, 201)
     angles = list(product(theta, phi))
     angles_array = np.array(angles)
-    
+
     # all L values
     for L_max, evens, rand_dist in [(1, False, False), (2, True, False), (3, False, True), (2, True, True)]:
         for trial in range(1):
             # make some random B_lms
-            sample_B_lms = np.zeros(2*[L_max + 1])
-            if True:
-                for L in range(L_max + 1):
-                    if  evens and L%2 == 1:
-                        continue
-                    for M in range(L+1):
-                        sample_B_lms[L, M] += rng.normal()
-            sample_B_lms[0, 0] = 1
-            
+            sample_B_lms = []
+
+            for L in range(0, L_max + 1):
+                if  evens and L%2 == 1:
+                    continue
+                for _ in range(L+1):
+                    sample_B_lms.append(rng.normal())
+            sample_B_lms[0] = 3
+            if not rand_dist:
+                sample_B_lms = len(sample_B_lms)*[0]
+                sample_B_lms[0] = 1
+            sample_B_lms = np.array(sample_B_lms)
+
             # get probabilities from B_lms
             angles_probs = fitting.Y_lms_distribution(angles_array[:,0], angles_array[:,1], L_max, sample_B_lms, evens)
             angles_probs = abs(angles_probs)
@@ -68,27 +72,15 @@ def test_SDA_for_Y_lm_distribution():
             samples_angles = angles_array[samples_indicies]
             samples_sph = np.hstack((np.ones([samples_angles.shape[0], 1]), samples_angles))
             samples_xyz = fitting.spherical_to_cart(samples_sph)
-            
-            # run SDA and get probability estimates
-            print("------------------\nrunning SDA trial = ", trial)
-            estimate_B_lms = fitting.SDA_Y_lms_distribution(samples_xyz, L_max, 0.0001, 1000, .1, evens)
+
+            # run least squares and get probability estimates
+            print("------------------\nrunning least squares trial = ", trial)
+            estimate_B_lms, _ = fitting.fit_Y_lms_binning_least_squares(samples_xyz, L_max, 100, evens)
             est_probs = fitting.Y_lms_distribution(angles_array[:,0], angles_array[:,1], L_max, estimate_B_lms, evens)
             est_probs /= sum(est_probs)
-            print(f"\nestimate_B_lms:\n{estimate_B_lms/estimate_B_lms[0,0]}")
-            print(f"sample_B_lms:\n{sample_B_lms}")
-            
+            print(f"\nnormed estimate_B_lms:\n{estimate_B_lms/estimate_B_lms[0]}")
+            print(f"normed sample_B_lms:\n{sample_B_lms/sample_B_lms[0]}")
+
             print("MSE of probs after opt = ", np.mean((est_probs - angles_probs)**2))
             assert np.all(est_probs >= 0)
-            assert np.mean((est_probs - angles_probs)**2) < 10**-4
-    assert False
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+            assert np.mean((est_probs - angles_probs)**2) < 10**-8

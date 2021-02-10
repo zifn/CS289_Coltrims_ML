fixed error in cross entropy calculation with minor tweeks to that test

src/fitting.py
@@ -198,8 +198,7 @@ def validation_cross_entropy(data_val_xyz, labels, model_params, L_max, only_eve
     assert data_val_xyz.shape[1] == 3
     data_val_sph = cart_to_spherical(data_val_xyz)
 
-    unique_labels = np.unique(labels)
-    unique_labels = list(range(len(model_params)))
+    unique_labels = np.unique(labels).astype(int)
 
     # make qs
     qs = []
@@ -208,13 +207,15 @@ def validation_cross_entropy(data_val_xyz, labels, model_params, L_max, only_eve
             pass
         qs.append(Y_lms_distribution(data_val_sph[:, 1], data_val_sph[:, 2],
                                                     L_max, model_params[label], only_even_Ls))
-    qs = np.array(qs).T
-    qs /= qs.sum(axis=1)[:, None] # normalization of probability density to probabilities of each class
+    if qs:
+        qs = np.array(qs).T
+        qs /= qs.sum(axis=1)[:, None] # normalization of probability density to probabilities of each class
 
     #compute cross-entropy
     flag = True
     cross_entropy = 0
     for label in unique_labels:
+        print(f"label = {label}")
         if label == -1:
             pass
         flag = False

src/tests/test_fitting.py
@@ -2,6 +2,7 @@ from itertools import product
 
 import numpy as np
 
+
 from .. import fitting
 
 def test_cart_spherical_conversions():
@@ -10,6 +11,7 @@ def test_cart_spherical_conversions():
     cardinal directions and if cart_to_spherical() and spherical_to_cart() are inverses
     if each other
     """
+    print(np.__version__)
     rng = np.random.default_rng()
 
     M_xyz_exp = np.array([[1, 0, 0],
@@ -121,5 +123,5 @@ def test_validation_cross_entropy():
     assert np.isclose(entropy, 0)
 
     #check no labels edge case
-    entropy2 = fitting.validation_cross_entropy(samples, [], model_params, L_max, only_even_Ls=False)
-    assert np.isclose(entropy2, np.inf), f"expected {np.inf} but recievied {entropy2}"
+    entropy = fitting.validation_cross_entropy(samples, [], model_params, L_max, only_even_Ls=False)
+    assert np.isclose(entropy, np.inf), f"expected {np.inf} but recievied {entropy}"